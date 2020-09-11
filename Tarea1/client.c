007e9cac07927d0bb73dfd8a6c215be74f5ad0f1
Arturo Cordoba <arturocv16@gmail.com>
2020-09-10 23:20:51 -0600

    Warning eliminado

--- Tarea1/client.c
+++ Tarea1/client.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <sys/types.h>
 #include <sys/socket.h>
-
 #include <netinet/in.h>
+#include <unistd.h>
 
 int main() {
     // Creacion del socket
@@ -15,17 +14,18 @@ int main() {
     serverAddr.sin_port = htons(8080);
     serverAddr.sin_addr.s_addr = INADDR_ANY;
 
+    // Se intenta conectar con el puerto de la direccion ip establecida
     int connectionStatus = connect(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
     // Verificar estado de conexion
     if (connectionStatus < 0) {
-        printf("Error al intentar conectar con el servidor");
+        printf("Error al intentar conectar con el servidor\n");
         return 0;
     }
 
     // Recibir respuesta del servidor
     char serverResponse[256];
     recv(clientSocket, &serverResponse, sizeof(serverResponse), 0);
-    printf("Respuesta recibida: %s\n", serverResponse);
+    printf("Respuesta: %s\n", serverResponse);
 
     // Se cierra la conexion con el servidor
     close(clientSocket);
